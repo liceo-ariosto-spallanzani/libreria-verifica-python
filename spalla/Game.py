6db7520f42ceef8bc71fc9f7beef8934e7649359
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-04-14 17:09:34 +0200

    first commit

--- spalla/Game.py
+++ spalla/Game.py
@@ -1,35 +1,36 @@
 import __main__ 
+import requests
 
 
 class Fg: 
-        rs="\033[00m"
-        black='\033[30m'
-        red='\033[31m'
-        green='\033[32m'
-        orange='\033[33m'
-        blue='\033[34m'
-        purple='\033[35m'
-        cyan='\033[36m'
-        lightgrey='\033[37m'
-        darkgrey='\033[90m'
-        lightred='\033[91m'
-        lightgreen='\033[92m'
-        yellow='\033[93m'
-        lightblue='\033[94m'
-        pink='\033[95m'
-        lightcyan='\033[96m'
+        rs = "\033[00m"
+        black = "\033[30m"
+        red = "\033[31m"
+        green = "\033[32m"
+        orange = "\033[33m"
+        blue = "\033[34m"
+        purple = "\033[35m"
+        cyan = "\033[36m"
+        lightgrey = "\033[37m"
+        darkgrey = "\033[90m"
+        lightred = "\033[91m"
+        lightgreen = "\033[92m"
+        yellow = "\033[93m"
+        lightblue = "\033[94m"
+        pink = "\033[95m"
+        lightcyan = "\033[96m"
 
 
 class Bg: 
-        rs="\033[00m"
-        black='\033[40m'
-        red='\033[41m'
-        green='\033[42m'
-        yellow='\033[43m'
-        blue='\033[44m'
-        magenta='\033[45m'
-        cyan='\033[46m'
-        white='\033[47m'
+        rs = "\033[00m"
+        black = "\033[40m"
+        red = "\033[41m"
+        green = "\033[42m"
+        yellow = "\033[43m"
+        blue = "\033[44m"
+        magenta = "\033[45m"
+        cyan = "\033[46m"
+        white = "\033[47m"
 
 
 class Directions:
@@ -55,34 +56,37 @@ class Entity:
 class Player(Entity):
     def __init__(self, game, x, y):
         self.tail = [(x, y)]
-        Entity.__init__(self, "*", game, x, y, Fg.blue, Bg.blue)
+        Entity.__init__(self, "*", game, x, y, Fg.red, Bg.blue)
         self.tail_color = self.bg_color
+        self.__moves = []
+
+    def get_moves(self):
+        return self.__moves
 
     def move(self, direction):
-        if self.game.status == Game.RUNNING:
+        if self.game.running:
             next_x = self.x
             next_y = self.y
             if direction == Directions.N and self.y > 0:
                 next_y -= 1
-            elif direction == Directions.S and self.y < self.game.h -1:
+            elif direction == Directions.S and self.y < self.game.h - 1:
                 next_y += 1
             elif direction == Directions.W and self.x > 0:
                 next_x -= 1
-            elif direction == Directions.E and self.x < self.game.w -1:
+            elif direction == Directions.E and self.x < self.game.w - 1:
                 next_x += 1
 
             e = self.game.get_entity_at_coords(next_x, next_y)
 
             self.x = next_x
             self.y = next_y
+            self.__moves.append((self.x, self.y))
             if e is None:
                 self.tail.append((self.x, self.y))
             else:
                 self.bg_color = e.bg_color
-                if type(e) is Obstacle:
-                    self.game.status = Game.GAME_OVER
-                elif type(e) is Gold:
-                    self.game.status = Game.WIN
+                if type(e) in (Obstacle, Gold):
+                    self.game.running = False
 
 
 class Obstacle(Entity):
@@ -95,28 +99,33 @@ class Gold(Entity):
         Entity.__init__(self, "$", game, x, y, Fg.yellow, Bg.yellow)
 
         
-class Game:
-    RUNNING = 0
-    GAME_OVER = 1
-    WIN = 2
-
+class Level:
     def __init__(self, level):
-        self.__level = level -1
-        self.__player = Player(self, 1, 1)
-        self.__entities = [self.__player]
-        self.__bg_color = Bg.green
-        self.status = Game.RUNNING
+        self.__level_index = level - 1
 
-        rows = ["#####\n", "#     #\n", "# $ #\n", "#####\n"]
+        res = requests.get("{}/levels".format(Game.url), headers={"Accept": "application/json"})
+        levels = res.json()
+        level_data = list(levels.values())[self.__level_index]
+        self.id = list(levels.keys())[self.__level_index]
+        self.description = level_data["description"]
+        self.max_moves = level_data["maxMoves"]
+        rows = level_data["map"].split("\n")
 
-        self.h = len(rows)
-        self.w = len(rows[0]) -1
+        self.__entities = []
+        self.__bg_color = Bg.white
+        self.running = True
+
+        self.h = len(rows) - 1
+        self.w = len(rows[0])
 
         for y in range(self.h):
             for x in range(self.w):
                 char = rows[y][x]
                 if char == "$":
                     self.__entities.append(Gold(self, x, y))
+                elif char == "*":
+                    self.__player = Player(self, x, y)
+                    self.__entities.insert(0, self.__player)
                 elif char != " ":
                     self.__entities.append(Obstacle(self, x, y))
 
@@ -137,28 +146,49 @@ class Game:
             if e.x == x and e.y == y: 
                 return e     
 
-    def go(self):
+    def play(self):
         print(self)
+        code = open(__main__.__file__).read()
+        r = requests.post("{}/solution/{}".format(Game.url, self.id), json={
+            "code": code,
+            "moves": self.__player.get_moves()
+        })
 
-        if self.status == Game.WIN:
-            print("{}Hai vinto!{}".format(Fg.green, Fg.rs))
-        elif self.status == Game.GAME_OVER:
-            print("{}Hai perso!{}".format(Fg.red, Fg.rs))
-
-        #print(open(__main__.__file__).read().count(".move_right()"))
+        message = r.json()["message"]
+        if r.status_code == 200:
+            print("{}{}{}".format(Fg.green, message, Fg.rs))
+        else:
+            print("{}{}{}".format(Fg.red, message, Fg.rs))
 
     def __str__(self):
-        out = ""
+        out = "{} (massimo {} mosse):\n".format(self.description, self.max_moves)
         for y in range(self.h):
             for x in range(self.w):
                 e = self.get_entity_at_coords(x, y)
                 if e is not None:
                     out += str(e)
                 elif (x, y) in self.__player.tail:
-                    out += "{}     {}".format(self.__player.tail_color, Bg.rs)
+                    out += "{}   {}".format(self.__player.tail_color, Bg.rs)
                 else:
-                    out += "{}     {}".format(self.__bg_color, Bg.rs)
+                    out += "{}   {}".format(self.__bg_color, Bg.rs)
 
             out += "\n"
 
         return out
+
+
+class Game:
+    url = "http://192.168.1.231:8080"
+
+    @staticmethod
+    def sign(name):
+        r = requests.post(
+            "{}/accreditamento".format(Game.url),
+            json={"name": name.upper()}
+        ).json()
+
+        print(r["message"])
+
+    @staticmethod
+    def start_level(number):
+        return Level(number)
--- test.py
+++ test.py
@@ -1,21 +1,13 @@
-'''
-from spalla import Verifica
-
-Verifica.url = "http://localhost:8080"
-Verifica.firma("Giovanni Bruno")
-
-es = Verifica.inizia_esercizio(3)
-
-print(es)
-'''
 
 from spalla.Game import Game
+Game.url = "http://localhost:8080"
 
-g = Game(1)
-g.move_right()
-#g.move_down()
-g.move_right()
-g.move_right()
-g.move_down()
-g.move_left()
-g.go()
+# Game.sign("Giovanni Bruno")
+
+level = Game.start_level(1)
+
+level.move_right()
+level.move_right()
+level.move_right()
+
+level.play()
