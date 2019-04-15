64c231d99a76ac4deedf045469e71b8eebfc0325
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-04-15 09:06:49 +0200

    mods

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
-
 from spalla.Game import Game
 Game.url = "http://localhost:8081"
 
 Game.sign("Giovanni Bruno")
 
-player = Game.start_level(4)
-
-
-player.move()
-player.move()
+player = Game.start_level(1)
 player.turn_left()
+player.move()
+player.turn_right()
+player.move()
 player.move()
 player.turn_right()
 player.move()
