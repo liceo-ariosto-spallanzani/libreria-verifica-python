aebdde1a8ce86e00c803faba72f50b47b36688ae
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-04-15 09:10:10 +0200

    mods

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='spalla',
-   version='1.2',
+   version='1.3',
    description='programma per verifiche Liceo Ariosto Spallanzani',
    license="MIT",
    long_description=long_description,
